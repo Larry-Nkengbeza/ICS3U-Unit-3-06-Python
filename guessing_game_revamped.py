7a02c010451993ea2144e76d36b5aa892ebeacbf
Ubuntu <ubuntu@ip-172-31-76-11.ec2.internal>
2020-12-09 17:22:13 +0000

    This is a guessing game revamped

--- guessing_game_revamped.py
+++ guessing_game_revamped.py
@@ -9,18 +9,19 @@ import random
 
 def main():
     # Input
-    user_guess = int(input("Enter a random INTEGER (-10-9): "))
+    user_guess_str = (input("Enter a random INTEGER (-10-9): "))
     computer_answer = random.randint(-10, 9)
     # Process and output
 
-    if user_guess == computer_answer:
-        print("Exactly right answer")
-    else:
-        print("Wrong number. The correct number is {0}"
-              .format(computer_answer))
     try:
-        user_guess = int(computer_answer)
-        print("You entered in the correct integer.")
+        # this might fail if they enter words
+        user_guess = int(user_guess_str)
+
+        if user_guess == computer_answer:
+            print("Exactly right answer")
+        else:
+            print("Wrong number. The correct number is {0}"
+                  .format(computer_answer))
     except Exception:
         print("This was not an integer.")
     finally:
